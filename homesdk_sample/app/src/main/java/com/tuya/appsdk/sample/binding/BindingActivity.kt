48b324d0ed11bd426644fad4e0b10509e16aa4c3
Guillermo Miravalles <miravallesg@gmail.com>
2022-11-16 19:09:23 -0300

    luego de login pide binding token

--- homesdk_sample/app/src/main/java/com/tuya/appsdk/sample/binding/BindingActivity.kt
+++ homesdk_sample/app/src/main/java/com/tuya/appsdk/sample/binding/BindingActivity.kt
@@ -1,10 +1,22 @@
 package com.tuya.appsdk.sample.binding
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.fastjson.JSON
 import com.tuya.appsdk.sample.R
+//okhttp libs
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response;
+import java.io.IOException
 
 
 class BindingActivity : AppCompatActivity() {
@@ -12,8 +24,44 @@ class BindingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_binding)
 
-         val token:String = intent.getStringExtra("token").toString()
+        val token: String = intent.getStringExtra("token").toString()
         Log.d("TAGGG binding", token)
+
+        //get binding token
+        //https://square.github.io/okhttp/recipes/
+
+        val client: OkHttpClient = OkHttpClient()
+        val url: String =
+            "https://tuyabff.apps.k8s.cablevision-labs.com.ar/tuya/device/paring/token"
+        val payload = " "
+        val requestBody = payload.toRequestBody()
+
+        val request = Request.Builder()
+            .header("User-Agent", "OkHttp Headers.java")
+            .addHeader("Authorization", "Bearer $token")
+            .addHeader("Accept", "application/json; q=0.5")
+            .method("POST", requestBody)
+            .url(url)
+            .build()
+
+             client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle this
+                Log.d("TAGGG", "error")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // Handle this
+
+                //log binding token response
+                Log.d("TAGGG", response.body!!.string())
+            }
+        })
+
+
+        //implement ap mode binding
+
 
     }
 }
+
